04e2a60488af854dd93b76b59863cfd0e55f5780
Yuriy Mironenko <ym@opun.ai>
2025-02-12 17:35:07 +0300

    Icon => Text in the header of RecepieScreen

--- lib/recipe_page/Receipe Description.dart
+++ lib/recipe_page/Receipe Description.dart
@@ -15,7 +15,7 @@ class RecepieScreen extends StatelessWidget {
 
     return
       Scaffold(
-        appBar: AppBar( title: Icon(Icons.fastfood),),
+        appBar: AppBar( title: Text("Рецепт")),
           body: Center(
                 child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 15),
