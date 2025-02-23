f99f2a64aeb8aa7c4a13a600dc16905ff8739d2a
Witch <vlv3113don@gmail.com>
2025-02-23 18:09:34 +0300

    Добавили кнопку в Shopping

--- lib/Shopping.dart
+++ lib/Shopping.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Shopping extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('123'),
+    return Center(child:
+    ElevatedButton(
+        onPressed: () {
+          // Делаем что-нибудь, когда кнопка нажата.
+        },
+        child: Text('Press me'),
+        //style: ElevatedButton.styleFrom(
+          //textStyle: TextStyle(fontSize: 20),
+          //backgroundColor: Colors.purple,
+          //foregroundColor: Colors.white,
+          //elevation: 5.0,
+          //padding: EdgeInsets.all(20),
+        //)
+    )
+
     );
   }
 }
